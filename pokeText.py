0a570b2dac1b7a8ab8edfb39ed284019af2d4558
Jim Peters <james.peters94@outlook.com>
2024-09-21 15:43:13 -0400

    Update to pokeText, using fetch functions from PokeApp

--- pokeText.py
+++ pokeText.py
@@ -3,21 +3,55 @@
 import pandas as pd
 from time import sleep
 
-pokelist = pd.read_csv('./pokemon.csv')
+pokelist = pd.read_csv('./pokemon.csv').fillna(" ")
 
 
-
-
-
+'''
 def fetch_poke(query):
     """queries the pandas dataframe using name, returns a series with data"""
     try:
-        if type(query) == int:
-            return pokelist.query('id == "{}"'.format(query)).values[0]
+        if type(int(query)) == int:
+            query = int(query)
+            return pokelist.query('id == @query').values[0]
         else:
-            return pokelist.query('species == "{}"'.format(query.lower())).values[0]
+            return pokelist.query("species == @query").values[0]
     except (IndexError, ValueError):
         print("I'm sorry, I cannot find that Pokémon.")
+        return [0]
+'''
+
+def process_search(query):
+    # Determine if search value is int or str
+    try:
+        query = int(query)
+        return fetch_id(query)
+    except ValueError:
+        return fetch_name(query)
+
+def fetch_name(query):
+    """queries the pandas dataframe by name, returns a PokeID"""
+    try:    
+        # Search for name, partial or otherwise
+        # Get series with true or false values dependant on the presence of the query in the dataframe
+        search_res = pokelist.species.str.contains(query.lower())
+        # Isolate the true values and convert to ID
+        poss_res = search_res[(search_res == True)].index +1
+        if len(poss_res) > 0:
+            return fetch_id(poss_res[0])
+        else:
+            print("Pokémon not found")
+            return [0]
+    except TypeError:   # Give up Looking
+        print("Pokémon not found")
+        return [0]
+
+def fetch_id(query):
+    """queries the pandas dataframe using ID, returns a series with data"""
+    # Search between max and min values of pokedex, allows for future expansion
+    if pokelist.id.min() <= query <= pokelist.id.max():
+        return pokelist.query('id == @query').values[0]
+    else:
+        print("Pokémon not found")
         return [0]
 
 def main():
@@ -29,7 +63,7 @@ def main():
 
     while choice != '0':
         print("""
-                1 - Search PokéText
+                1 - Search Pokédex
                 2 - Previous Pokémon
                 3 - Next Pokémon
                 0 - Exit PokéText
@@ -39,13 +73,13 @@ def main():
 
         if choice == '1':
             q = input("\n\t Search by Name or ID: ").lower()
-            pokemon = fetch_poke(q)
+            pokemon = process_search(q)
             id = pokemon[0]
         elif choice == '2':
-            pokemon = fetch_poke(id-1)
+            pokemon = fetch_id(id-1)
             id = pokemon[0]
         elif choice == '3':
-            pokemon = fetch_poke(id+1)
+            pokemon = fetch_id(id+1)
             id = pokemon[0]
         elif choice == '0':
             break
